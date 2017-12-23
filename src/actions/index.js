6e8c061b922efcba26a37c6bbea9b76c19c4c2ca
Tim Rourke <tim@timrourke.com>
2017-12-23 15:00:19 -0600

    Add action creators

---
+++ src/actions/index.js
@@ -0,0 +1,232 @@
+import { v4 } from 'uuid';
+
+// Project action types
+export const CREATE_PROJECT = Symbol('CREATE_PROJECT');
+export const UPDATE_PROJECT = Symbol('UPDATE_PROJECT');
+export const DELETE_PROJECT = Symbol('DELETE_PROJECT');
+
+// Board action types
+export const CREATE_BOARD   = Symbol('CREATE_BOARD');
+export const UPDATE_BOARD   = Symbol('UPDATE_BOARD');
+export const DELETE_BOARD   = Symbol('DELETE_BOARD');
+
+// Column action types
+export const CREATE_COLUMN  = Symbol('CREATE_COLUMN');
+export const UPDATE_COLUMN  = Symbol('UPDATE_COLUMN');
+export const DELETE_COLUMN  = Symbol('DELETE_COLUMN');
+
+// Row action types
+export const CREATE_ROW     = Symbol('CREATE_ROW');
+export const UPDATE_ROW     = Symbol('UPDATE_ROW');
+export const DELETE_ROW     = Symbol('DELETE_ROW');
+
+// Card action types
+export const CREATE_CARD    = Symbol('CREATE_CARD');
+export const UPDATE_CARD    = Symbol('UPDATE_CARD');
+export const DELETE_CARD    = Symbol('DELETE_CARD');
+
+/**
+ * Create a new project
+ *
+ * @param {String} title
+ * @return {Object}
+ */
+export const createProject = (title) => ({
+  type: CREATE_PROJECT,
+  payload: {
+    id: v4(),
+    title,
+  },
+});
+
+/**
+ * Update a project
+ *
+ * @param {String} id
+ * @param {String} title
+ * @return {Object}
+ */
+export const updateProject = (id, title) => ({
+  type: UPDATE_PROJECT,
+  payload: {
+    id,
+    title,
+  },
+});
+
+/**
+ * Delete a project
+ *
+ * @param {String} id
+ * @return {Object}
+ */
+export const deleteProject = (id) => ({
+  type: DELETE_PROJECT,
+  payload: {id},
+});
+
+/**
+ * Create a new board
+ *
+ * @param {String} title
+ * @return {Object}
+ */
+export const createBoard = (title) => ({
+  type: CREATE_BOARD,
+  payload: {
+    id: v4(),
+    title,
+  },
+});
+
+/**
+ * Update a board
+ *
+ * @param {String} id
+ * @param {String} title
+ * @return {Object}
+ */
+export const updateBoard = (id, title) => ({
+  type: UPDATE_BOARD,
+  payload: {
+    id,
+    title,
+  },
+});
+
+/**
+ * Delete a board
+ *
+ * @param {String} id
+ * @return {Object}
+ */
+export const deleteBoard = (id) => ({
+  type: DELETE_BOARD,
+  payload: {id},
+});
+
+/**
+ * Create a new column
+ *
+ * @param {String} title
+ * @return {Object}
+ */
+export const createColumn = (title) => ({
+  type: CREATE_COLUMN,
+  payload: {
+    id: v4(),
+    title,
+  },
+});
+
+/**
+ * Update a column
+ *
+ * @param {String} id
+ * @param {String} title
+ * @param {Number} order
+ * @return {Object}
+ */
+export const updateColumn = (id, title, order) => ({
+  type: UPDATE_COLUMN,
+  payload: {
+    id,
+    title,
+    order,
+  },
+});
+
+/**
+ * Delete a column
+ *
+ * @param {String} id
+ * @return {Object}
+ */
+export const deleteColumn = (id) => ({
+  type: DELETE_COLUMN,
+  payload: {id},
+});
+
+/**
+ * Create a new row
+ *
+ * @param {String} title
+ * @return {Object}
+ */
+export const createRow = (title) => ({
+  type: CREATE_ROW,
+  payload: {
+    id: v4(),
+    title,
+  },
+});
+
+/**
+ * Update a row
+ *
+ * @param {String} id
+ * @param {String} title
+ * @param {Number} order
+ * @return {Object}
+ */
+export const updateRow = (id, title, order) => ({
+  type: UPDATE_ROW,
+  payload: {
+    id,
+    title,
+    order,
+  },
+});
+
+/**
+ * Delete a row
+ *
+ * @param {String} id
+ * @return {Object}
+ */
+export const deleteRow = (id) => ({
+  type: DELETE_ROW,
+  payload: {id},
+});
+
+/**
+ * Create a new card
+ *
+ * @param {String} title
+ * @return {Object}
+ */
+export const createCard = (title) => ({
+  type: CREATE_CARD,
+  payload: {
+    id: v4(),
+    title,
+  },
+});
+
+/**
+ * Update a card
+ *
+ * @param {String} id
+ * @param {String} title
+ * @param {Number} order
+ * @return {Object}
+ */
+export const updateCard = (id, title, order) => ({
+  type: UPDATE_CARD,
+  payload: {
+    id,
+    title,
+    order,
+  },
+});
+
+/**
+ * Delete a card
+ *
+ * @param {String} id
+ * @return {Object}
+ */
+export const deleteCard = (id) => ({
+  type: DELETE_CARD,
+  payload: {id},
+});
---
+++ src/actions/index.test.js
@@ -0,0 +1,119 @@
+import * as actions from './index';
+
+it('should create action for "createProject"', () => {
+  const action = actions.createProject('foo');
+
+  expect(action.type).toBe(actions.CREATE_PROJECT);
+  expect(action.payload.id).toBeDefined();
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "updateProject"', () => {
+  const action = actions.updateProject('some-id', 'foo');
+
+  expect(action.type).toBe(actions.UPDATE_PROJECT);
+  expect(action.payload.id).toBe('some-id');
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "deleteProject"', () => {
+  const action = actions.deleteProject('some-id');
+
+  expect(action.type).toBe(actions.DELETE_PROJECT);
+  expect(action.payload.id).toBe('some-id');
+});
+
+it('should create action for "createBoard"', () => {
+  const action = actions.createBoard('foo');
+
+  expect(action.type).toBe(actions.CREATE_BOARD);
+  expect(action.payload.id).toBeDefined();
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "updateBoard"', () => {
+  const action = actions.updateBoard('some-id', 'foo');
+
+  expect(action.type).toBe(actions.UPDATE_BOARD);
+  expect(action.payload.id).toBe('some-id');
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "deleteBoard"', () => {
+  const action = actions.deleteBoard('some-id');
+
+  expect(action.type).toBe(actions.DELETE_BOARD);
+  expect(action.payload.id).toBe('some-id');
+});
+
+it('should create action for "createColumn"', () => {
+  const action = actions.createColumn('foo');
+
+  expect(action.type).toBe(actions.CREATE_COLUMN);
+  expect(action.payload.id).toBeDefined();
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "updateColumn"', () => {
+  const action = actions.updateColumn('some-id', 'foo', 7);
+
+  expect(action.type).toBe(actions.UPDATE_COLUMN);
+  expect(action.payload.id).toBe('some-id');
+  expect(action.payload.title).toBe('foo');
+  expect(action.payload.order).toBe(7);
+});
+
+it('should create action for "deleteColumn"', () => {
+  const action = actions.deleteColumn('some-id');
+
+  expect(action.type).toBe(actions.DELETE_COLUMN);
+  expect(action.payload.id).toBe('some-id');
+});
+
+it('should create action for "createRow"', () => {
+  const action = actions.createRow('foo');
+
+  expect(action.type).toBe(actions.CREATE_ROW);
+  expect(action.payload.id).toBeDefined();
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "updateRow"', () => {
+  const action = actions.updateRow('some-id', 'foo', 7);
+
+  expect(action.type).toBe(actions.UPDATE_ROW);
+  expect(action.payload.id).toBe('some-id');
+  expect(action.payload.title).toBe('foo');
+  expect(action.payload.order).toBe(7);
+});
+
+it('should create action for "deleteRow"', () => {
+  const action = actions.deleteRow('some-id');
+
+  expect(action.type).toBe(actions.DELETE_ROW);
+  expect(action.payload.id).toBe('some-id');
+});
+
+it('should create action for "createCard"', () => {
+  const action = actions.createCard('foo');
+
+  expect(action.type).toBe(actions.CREATE_CARD);
+  expect(action.payload.id).toBeDefined();
+  expect(action.payload.title).toBe('foo');
+});
+
+it('should create action for "updateCard"', () => {
+  const action = actions.updateCard('some-id', 'foo', 7);
+
+  expect(action.type).toBe(actions.UPDATE_CARD);
+  expect(action.payload.id).toBe('some-id');
+  expect(action.payload.title).toBe('foo');
+  expect(action.payload.order).toBe(7);
+});
+
+it('should create action for "deleteCard"', () => {
+  const action = actions.deleteCard('some-id');
+
+  expect(action.type).toBe(actions.DELETE_CARD);
+  expect(action.payload.id).toBe('some-id');
+});
