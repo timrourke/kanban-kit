fe1a8b8933d3ef158089fb4c0e4fa41f3bd2060e
Tim Rourke <tim@timrourke.com>
2018-01-21 07:48:48 -0600

    Install flexboxgrid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './../node_modules/flexboxgrid/dist/flexboxgrid.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
