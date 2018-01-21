7a6d0d174487ea118db948fec8bc133fbe35d8d6
Tim Rourke <tim@timrourke.com>
2018-01-21 09:10:26 -0600

    Render a random number of cards for now

--- src/components/Columns.jsx
+++ src/components/Columns.jsx
@@ -5,46 +5,29 @@ class Columns extends Component {
   render() {
     return (
       <div className="Columns-wrap">
-        <ul className="Column">
-          <li>
-            <Card />
-          </li>
-        </ul>
-        <ul className="Column">
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-        </ul>
-        <ul className="Column">
-          <li>
-            <Card />
-          </li>
-        </ul>
-        <ul className="Column">
-          <li>
-            <Card />
-          </li>
-        </ul>
-        <ul className="Column">
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-        </ul>
+        {this.props.columns.map((column, index) => {
+          const numCardsToRender = Math.floor(Math.random() * 10) + 1;
+          const cards = [];
+
+          for (let i = 0; i < numCardsToRender; i++) {
+            cards.push(
+              <li
+                key={`column-${column.id}-column-${index+1}-card-${i+1}`}
+              >
+                <Card />
+              </li>
+            );
+          }
+
+          return (
+            <ul
+              className="Column"
+              key={`column-${column.id}-column-${index+1}`}
+            >
+              {cards}
+            </ul>
+          );
+        })}
       </div>
     );
   }
